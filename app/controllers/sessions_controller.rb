1579cb432ead813cb62c9e7e5412459cf964ec00
Hotson lu <learning2010@gmail.com>
2016-12-17 16:50:13 +0800

    add bootstrap , twetter gem

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
-    raise :test
+    # raise :test
     session[:user_id] = @user.id
     redirect_to root_path
   end
